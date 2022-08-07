89bc60125cf31739a98a287b90cc5a56735ad44a
Halil İbrahim Özdoğan <hiozdogan2@gmail.com>
2022-08-07 15:20:09 +0200

    calculation of max and min fall by syombol seperated from the router

--- src/routes/Stock/v1/index.ts
+++ src/routes/Stock/v1/index.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { Request, Response } from 'express';
 import { TradeRepository } from '@repositories';
-import { RoundNumber } from '@utils';
+import calculateMaxMinFallBySymbol from './calculateMaxMinFallBySymbol';
 
 export default function (route: Router) {
     const tradeRepository = new TradeRepository();
@@ -40,77 +40,7 @@ export default function (route: Router) {
             const { start: startDate, end: endDate } = req.query as { start: string, end: string };
 
             const trades = await tradeRepository.findBetweenDates({ startDate, endDate });
-
-            const previousPriceBySymbol: Record<string, number> = {};
-            const risesBySymbol: Record<string, number[]> = {};
-            const fallsBySymbol: Record<string, number[]> = {};
-            const fluctuationsBySymbol: Record<string, { count: number, isPricePreviouslyIncreasing: boolean|null }> = {};
-            const statsBySymbol: Record<string, { fluctuations: number, max_rise: number, max_fall: number }> = {};
-
-            trades.forEach((trade) => {
-                if (!risesBySymbol[trade.symbol]) {
-                    risesBySymbol[trade.symbol] = [];
-                }
-
-                if (!fallsBySymbol[trade.symbol]) {
-                    fallsBySymbol[trade.symbol] = [];
-                }
-
-                if (!fluctuationsBySymbol[trade.symbol]) {
-                    fluctuationsBySymbol[trade.symbol] = { count: 0, isPricePreviouslyIncreasing: null };
-                }
-
-                if (previousPriceBySymbol[trade.symbol] !== undefined) {
-                    // Increasing fluctuation detection
-                    if (trade.price > previousPriceBySymbol[trade.symbol]) {
-                        if (fluctuationsBySymbol[trade.symbol].isPricePreviouslyIncreasing === false) {
-                            fluctuationsBySymbol[trade.symbol].count++;
-                        }
-
-                        fluctuationsBySymbol[trade.symbol].isPricePreviouslyIncreasing = true;
-                    }
-
-                    // Decreasing fluctuation detection
-                    if (trade.price < previousPriceBySymbol[trade.symbol]) {
-                        if (fluctuationsBySymbol[trade.symbol].isPricePreviouslyIncreasing === true) {
-                            fluctuationsBySymbol[trade.symbol].count++;
-                        }
-
-                        fluctuationsBySymbol[trade.symbol].isPricePreviouslyIncreasing = false;
-                    }
-
-                    const difference = trade.price - previousPriceBySymbol[trade.symbol];
-
-                    if (difference > 0) {
-                        risesBySymbol[trade.symbol].push(Math.abs(difference));
-                    } else {
-                        fallsBySymbol[trade.symbol].push(Math.abs(difference));
-                    }
-                }
-
-                previousPriceBySymbol[trade.symbol] = trade.price;
-                statsBySymbol[trade.symbol] = { fluctuations: 0, max_fall: 0, max_rise: 0 };
-            });
-
-            const stats = [];
-            for (const symbol in statsBySymbol) {
-                const fluctuations = fluctuationsBySymbol[symbol].count;
-                const maxRise = RoundNumber(Math.max(...risesBySymbol[symbol]), 2);
-                const maxFall = RoundNumber(Math.max(...fallsBySymbol[symbol]), 2);
-
-                if (!fluctuations) {
-                    stats.push({
-                        stock: symbol,
-                        message: 'There are no trades in the given date range'
-                    });
-                } else {
-                    stats.push({
-                        stock: symbol,
-                        max_rise: maxRise,
-                        max_fall: maxFall,
-                    });
-                }
-            }
+            const stats = calculateMaxMinFallBySymbol(trades);
 
             res.status(200).send(stats.sort((a,b) => a.stock.localeCompare(b.stock)));
         } catch (e) {
